a5d49181b642b2e9c9e863b934ecdb431824f00b
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-03 13:46:30 -0400

    minor changes

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
 var game = {
-    tickspeed: 1,
+    tickspeed: 100,
     health: 100,
 };
 
 const gameloop = setInterval(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     spawnSlime(0, 0, 0);
-}, 100);
+}, game.tickspeed);
