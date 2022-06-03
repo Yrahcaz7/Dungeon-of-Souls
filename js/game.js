7f24d2e25c29e0ea49bf49fc5290438a19aa0372
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-03 16:04:40 -0400

    enemy spawning

--- js/game.js
+++ js/game.js
@@ -1,10 +1,18 @@
 var game = {
     tickspeed: 100,
     health: 100,
+    floor: 1,
+    inBattle: false,
+    enemies: [],
 };
 
 const gameloop = setInterval(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    spawnSlime(200, 20);
-    drawHealth(200, 84, 90);
+    if (game.floor == 1) {
+        if (!game.inBattle) {
+            game.enemies.push(['slime', 20, 20]);
+            game.inBattle = true;
+        };
+    };
+    spawnEnemies();
 }, game.tickspeed);
--- js/images.js
+++ js/images.js
@@ -8,6 +8,53 @@ slime.src = "images/slime.png";
 
 healthBar.src = "images/healthBar.png";
 
+function spawnEnemies() {
+	for (let a = 0; a < game.enemies.length; a++) {
+		enemy = game.enemies[a];
+		if (game.enemies.length == 1) {
+			x = 210;
+			y = 20;
+		} else if (game.enemies.length == 2) {
+			if (a == 0) {
+				x = 230;
+				y = 10;
+			} else {
+				x = 160;
+				y = 50;
+			};
+		} else if (game.enemies.length == 3) {
+			if (a == 0) {
+				x = 230;
+				y = 30;
+			} else if (a == 2) {
+				x = 160;
+				y = 70;
+			} else {
+				x = 160;
+				y = 5;
+			};
+		} else if (game.enemies.length == 4) {
+			if (a == 0) {
+				x = 230;
+				y = 5;
+			} else if (a == 2) {
+				x = 230;
+				y = 70;
+			} else if (a == 3) {
+				x = 160;
+				y = 70;
+			} else {
+				x = 160;
+				y = 5;
+			};
+		};
+		if (enemy[0] == 'slime') {
+			spawnSlime(x, y);
+			drawHealth(x, y + 64, enemy[2] / enemy[1] * 100);
+		};
+	};
+};
+
 function spawnSlime(x, y) {
 	if (slimeFrame >= 4) slimeFrame = 0;
 	ctx.drawImage(slime, Math.floor(slimeFrame) * 64, 0, 64, 64, x, y, 64, 64);
