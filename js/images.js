a89f4e6ae44f2fd369b5b9e67b94cd100c95ce15
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-03 11:33:51 -0400

    Create images.js

---
+++ js/images.js
@@ -0,0 +1,24 @@
+const slime = new Image;
+
+slime.src = "Slime-Sheet.png";
+
+function spawnSlime(x, y, frame) {
+	ctx.mozImageSmoothingEnabled = false;
+  	ctx.webkitImageSmoothingEnabled = false;
+  	ctx.msImageSmoothingEnabled = false;
+  	ctx.imageSmoothingEnabled = false;
+	ctx.drawImage(
+		// load image
+		slime,
+		// get frame
+		frame * 64,
+		0,
+		64,
+		64,
+		// print frame
+		x,
+		y,
+		64 * 4,
+		64 * 4
+	);
+};
