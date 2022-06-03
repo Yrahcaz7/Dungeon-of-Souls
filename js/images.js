8571e11bd05906eebf68772adfc9915bb6258331
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-03 13:46:17 -0400

    fix image

--- js/images.js
+++ js/images.js
@@ -3,10 +3,6 @@ const slime = new Image;
 slime.src = "images/slime.png";
 
 function spawnSlime(x, y, frame) {
-	ctx.mozImageSmoothingEnabled = false;
-  	ctx.webkitImageSmoothingEnabled = false;
-  	ctx.msImageSmoothingEnabled = false;
-  	ctx.imageSmoothingEnabled = false;
 	ctx.drawImage(
 		// load image
 		slime,
@@ -18,7 +14,7 @@ function spawnSlime(x, y, frame) {
 		// print frame
 		x,
 		y,
-		64 * 4,
-		64 * 4
+		64,
+		64
 	);
 };
