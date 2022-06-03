d6cff6b26a7b65253004e52601daabc6f45db554
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-03 15:25:11 -0400

    draw health bar

--- js/game.js
+++ js/game.js
@@ -6,4 +6,5 @@ var game = {
 const gameloop = setInterval(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     spawnSlime(200, 20);
+    drawHealth(200, 84, 90);
 }, game.tickspeed);
--- js/images.js
+++ js/images.js
@@ -1,11 +1,22 @@
 const slime = new Image;
 
+const healthBar = new Image;
+
 var slimeFrame = 0;
 
 slime.src = "images/slime.png";
+
+healthBar.src = "images/healthBar.png";
 
 function spawnSlime(x, y) {
 	if (slimeFrame >= 4) slimeFrame = 0;
 	ctx.drawImage(slime, Math.floor(slimeFrame) * 64, 0, 64, 64, x, y, 64, 64);
 	slimeFrame += 0.1;
+};
+
+function drawHealth(x, y, percentage) {
+	if (percentage < 0) frame = 0;
+	else if (percentage > 100) frame = 62;
+	else frame = percentage * 0.62;
+	ctx.drawImage(healthBar, 0, Math.round(frame) * 12, 64, 13, x, y, 64, 13);
 };
