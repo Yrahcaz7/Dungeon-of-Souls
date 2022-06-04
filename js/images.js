2418e23695f6384b94ab162d249db07464c3e80a
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-04 14:40:42 -0400

    simplified numbers to lore

--- js/images.js
+++ js/images.js
@@ -2,7 +2,7 @@ const slime = new Image, smallSlime = new Image, // monsters
 
 background = new Image, clock = new Image, // backrounds
 
-healthBar = new Image, numbers = new Image, letters = new Image, view = new Image; // other
+healthBar = new Image, letters_black = new Image, letters_red = new Image, view = new Image; // other
 
 var backAnim = [0, "up"], enemyAnim = [0, 1, 2, 3];
 
@@ -16,9 +16,9 @@ clock.src = "images/clock.png";
 
 healthBar.src = "images/healthBar.png";
 
-numbers.src = "images/numbers.png";
+letters_black.src = "images/letters_black.png";
 
-letters.src = "images/letters.png";
+letters_red.src = "images/letters_red.png";
 
 view.src = "images/view.png";
 
@@ -75,61 +75,35 @@ function spawnEnemies() {
 			};
 		};
 		if (enemyAnim[a] >= 4) enemyAnim[a] = 0;
-		if (enemy[0] == "slime") ctx.drawImage(slime, Math.floor(enemyAnim[a]) * 64, 0, 64, 64, x, y, 64, 64);
-		if (enemy[0] == "smallSlime") ctx.drawImage(smallSlime, Math.floor(enemyAnim[a]) * 64, 0, 64, 64, x, y, 64, 64);
+		if (enemy[0] == "slime") {
+			ctx.drawImage(slime, Math.floor(enemyAnim[a]) * 64, 0, 64, 64, x, y, 64, 64);
+		} else if (enemy[0] == "smallSlime") {
+			ctx.drawImage(smallSlime, Math.floor(enemyAnim[a]) * 64, 0, 64, 64, x, y, 64, 64);
+		};
 		enemyAnim[a] += (Math.random() + 0.5) * 0.1;
 		percentage = enemy[1] / enemy[2];
 		if (percentage < 0) frame = 0;
 		else if (percentage > 1) frame = 62;
 		else frame = percentage * 62;
 		ctx.drawImage(healthBar, 0, Math.round(frame) * 11, 64, 12, x, y + 64, 64, 12);
-		drawNumber(x + 25, y + 66, enemy[1], "left");
-		drawNumber(x + 34, y + 66, enemy[2], "right");
+		drawLore(x + 25, y + 66, enemy[1], "black", "left");
+		drawLore(x + 34, y + 66, enemy[2], "black", "right");
 	};
 	ctx.drawImage(view, 0, 0);
-	drawLore(1, 1, "floor: " + game.floor);
+	drawLore(1, 1, "floor: " + game.floor, "red", "right");
 };
 
-function drawNumber(x, y, num, position = "right") {
-	num = Math.round(num);
-	if (position == "right") {
-		if (num >= 1000) {
-			ctx.drawImage(numbers, Math.floor(num / 1000) * 6, 0, 5, 8, x, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 100 % 10) * 6, 0, 5, 8, x + 6, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 10 % 10) * 6, 0, 5, 8, x + 12, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x + 18, y, 5, 8);
-		} else if (num >= 100) {
-			ctx.drawImage(numbers, Math.floor(num / 100) * 6, 0, 5, 8, x, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 10 % 10) * 6, 0, 5, 8, x + 6, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x + 12, y, 5, 8);
-		} else if (num >= 10) {
-			ctx.drawImage(numbers, Math.floor(num / 10) * 6, 0, 5, 8, x, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x + 6, y, 5, 8);
-		} else {
-			ctx.drawImage(numbers, num * 6, 0, 5, 8, x, y, 5, 8);
-		};
-	} else if (position == "left") {
-		if (num >= 1000) {
-			ctx.drawImage(numbers, Math.floor(num / 1000) * 6, 0, 5, 8, x - 18, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 100 % 10) * 6, 0, 5, 8, x - 12, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 10 % 10) * 6, 0, 5, 8, x - 6, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x, y, 5, 8);
-		} else if (num >= 100) {
-			ctx.drawImage(numbers, Math.floor(num / 100) * 6, 0, 5, 8, x - 12, y, 5, 8);
-			ctx.drawImage(numbers, Math.floor(num / 10 % 10) * 6, 0, 5, 8, x - 6, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x, y, 5, 8);
-		} else if (num >= 10) {
-			ctx.drawImage(numbers, Math.floor(num / 10) * 6, 0, 5, 8, x - 6, y, 5, 8);
-			ctx.drawImage(numbers, num % 10 * 6, 0, 5, 8, x, y, 5, 8);
-		} else {
-			ctx.drawImage(numbers, num * 6, 0, 5, 8, x, y, 5, 8);
-		};
-	};
-};
-
-function drawLore(x, y, string) {
+function drawLore(x, y, string, color = "black", position = "right") {
+	if (string === null || string === undefined || string === NaN) return;
+	string = "" + string;
 	for (let a = 0; a < string.length; a++) {
 		index = string.charCodeAt(a);
-		ctx.drawImage(letters, (index - 32) * 6, 0, 5, 10, x + (a * 6), y, 5, 10);
+		if (color == "red") img = letters_red;
+		else img = letters_black;
+		if (position == "right") {
+			ctx.drawImage(img, (index - 32) * 6, 0, 5, 10, x + (a * 6), y, 5, 10);
+		} else if (position == "left") {
+			ctx.drawImage(img, (index - 32) * 6, 0, 5, 10, x + ((a - string.length + 1) * 6), y, 5, 10);
+		};
 	};
 };
