2a1b56b41456d398c42515ea80f6247df7fa7ca5
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-04 17:22:17 -0400

    ruins more destroyed & arch floats

--- js/images.js
+++ js/images.js
@@ -1,16 +1,18 @@
 const slime_big = new Image, slime_small = new Image, // monsters
 
-background = new Image, clock = new Image, // backrounds
+background = new Image, floating_arch = new Image, clock = new Image, // backrounds
 
 health_bar = new Image, letters_black = new Image, letters_red = new Image, view = new Image; // other
 
-var backAnim = [0, "up"], enemyAnim = [0, 1, 2, 3];
+var backAnim = [0, "up", 0.5, "down"], enemyAnim = [0, 1, 2, 3];
 
 slime_big.src = "images/slime_big.png";
 
 slime_small.src = "images/slime_small.png";
 
 background.src = "images/background.png";
+
+floating_arch.src = "images/floating_arch.png";
 
 clock.src = "images/clock.png";
 
@@ -24,11 +26,16 @@ view.src = "images/view.png";
 
 function spawnEnemies() {
 	ctx.drawImage(background, 0, 0);
-	ctx.drawImage(clock, 120, 28 - Math.round(backAnim[0]));
+	ctx.drawImage(floating_arch, 86, 10 - Math.round(backAnim[0]));
+	ctx.drawImage(clock, 120, 28 - Math.round(backAnim[2]));
 	if (backAnim[0] >= 1) backAnim[1] = "down";
 	else if (backAnim[0] <= -1) backAnim[1] = "up";
 	if (backAnim[1] == "up") backAnim[0] += (Math.random() + 0.5) * 0.075;
 	else if (backAnim[1] == "down") backAnim[0] -= (Math.random() + 0.5) * 0.075;
+	if (backAnim[2] >= 1) backAnim[3] = "down";
+	else if (backAnim[2] <= -1) backAnim[3] = "up";
+	if (backAnim[3] == "up") backAnim[2] += (Math.random() + 0.5) * 0.075;
+	else if (backAnim[3] == "down") backAnim[2] -= (Math.random() + 0.5) * 0.075;
 	if (game.enemies.length > 4) {
 		for (let a = 4; a < game.enemies.length; a++) {
 			game.hiddenEnemies[a - 4] = game.enemies[a];
