10db221e3ea95f325f512e920b1bd49fdce549c1
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-05 09:57:12 -0400

    bigger canvas, improved things

--- js/canvas.js
+++ js/canvas.js
@@ -5,6 +5,8 @@ function canvasData() {
 	let canv = document.getElementById("canvas");
 	if (canv === undefined || canv === null) return false;
 	canvas = canv;
+	width = canvas.width;
+	height = canvas.height;
 	ctx = canvas.getContext("2d");
 	return true;
 };
--- js/images.js
+++ js/images.js
@@ -1,10 +1,7 @@
 const slime_big = new Image, slime_small = new Image, // monsters
-
-background = new Image, floating_arch = new Image, // backrounds
-
-clock_face = new Image, clock_hour_hand = new Image, clock_min_hand = new Image, clock_node = new Image, // clock
-
-health_bar = new Image, letters_black = new Image, letters_red = new Image, view = new Image; // other
+	background = new Image, floating_arch = new Image, // backrounds
+	clock_face = new Image, clock_hour_hand = new Image, clock_min_hand = new Image, clock_node = new Image, // the clock
+	health_bar = new Image, letters_black = new Image, letters_red = new Image, view = new Image; // other
 
 var backAnim = [0, "up", 0.5, "down", 0, 0], enemyAnim = [0, 1, 2, 3];
 
@@ -37,18 +34,19 @@ const HourConvert = 82 / 12, // number of frames divided by number of hours on a
 MinConvert = 80 / 60; // number of frames divided by number of minutes in an hour
 
 function renderRoom() {
-	var now = new Date(Date.now());
-	var time = [now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()];
+	var now = new Date(Date.now()),
+		time = [now.getHours(), now.getMinutes(), now.getSeconds(), now.getMilliseconds()],
+		centerY = height / 2 - 50;
 	time[2] += (time[3] / 1000);
 	time[1] += (time[2] / 60);
 	time[0] += (time[1] / 60);
 	if (time[0] >= 12) time[0] = time[0] - 12;
 	ctx.drawImage(background, 0, 0);
-	ctx.drawImage(floating_arch, 86, 10 - Math.round(backAnim[0]));
-	ctx.drawImage(clock_face, 120, 28 - Math.round(backAnim[2]));
-	ctx.drawImage(clock_hour_hand, Math.floor((time[0]) * HourConvert) * 24, 0, 24, 24, 138, 46 - Math.round(backAnim[2]), 24, 24);
-	ctx.drawImage(clock_min_hand, Math.floor((time[1]) * MinConvert) * 34, 0, 34, 34, 133, 41 - Math.round(backAnim[2]), 34, 34);
-	ctx.drawImage(clock_node, 146, 54 - Math.round(backAnim[2]));
+	ctx.drawImage(floating_arch, 136, 42 - Math.round(backAnim[0]));
+	ctx.drawImage(clock_face, 170, 71 - Math.round(backAnim[2]));
+	ctx.drawImage(clock_hour_hand, Math.floor((time[0]) * HourConvert) * 24, 0, 24, 24, 188, 89 - Math.round(backAnim[2]), 24, 24);
+	ctx.drawImage(clock_min_hand, Math.floor((time[1]) * MinConvert) * 34, 0, 34, 34, 183, 84 - Math.round(backAnim[2]), 34, 34);
+	ctx.drawImage(clock_node, 196, 97 - Math.round(backAnim[2]));
 	if (backAnim[0] >= 1) backAnim[1] = "down";
 	else if (backAnim[0] <= -1) backAnim[1] = "up";
 	if (backAnim[1] == "up") backAnim[0] += (Math.random() + 0.5) * 0.075;
@@ -57,51 +55,62 @@ function renderRoom() {
 	else if (backAnim[2] <= -1) backAnim[3] = "up";
 	if (backAnim[3] == "up") backAnim[2] += (Math.random() + 0.5) * 0.075;
 	else if (backAnim[3] == "down") backAnim[2] -= (Math.random() + 0.5) * 0.075;
-	if (game.enemies.length > 4) {
-		for (let a = 4; a < game.enemies.length; a++) {
-			game.hiddenEnemies[a - 4] = game.enemies[a];
+	if (game.enemies.length > 6) {
+		for (let a = 6; a < game.enemies.length; a++) {
+			game.hiddenEnemies[a - 6] = game.enemies[a];
 			game.enemies.splice(a, 1);
 		};
 	};
 	for (let a = 0; a < game.enemies.length; a++) {
 		enemy = game.enemies[a];
+		if (a == 0) {
+			x = width - 70;
+			y = centerY;
+		} else if (a == 1) {
+			x = width - 140;
+			y = centerY + 32;
+		} else if (a == 2) {
+			x = width - 140;
+			y = centerY - 32;
+		};
 		if (game.enemies.length == 1) {
-			x = 210;
-			y = 20;
+			x = width - 110;
+			y = centerY;
 		} else if (game.enemies.length == 2) {
 			if (a == 0) {
-				x = 230;
-				y = 10;
-			} else {
-				x = 160;
-				y = 50;
-			};
-		} else if (game.enemies.length == 3) {
-			if (a == 0) {
-				x = 230;
-				y = 30;
-			} else if (a == 2) {
-				x = 160;
-				y = 70;
-			} else {
-				x = 160;
-				y = 5;
+				x = width - 70;
+				y = centerY - 5;
+			} else if (a == 1) {
+				x = width - 140;
+				y = centerY + 20;
 			};
 		} else if (game.enemies.length == 4) {
-			if (a == 0) {
-				x = 230;
-				y = 5;
-			} else if (a == 2) {
-				x = 230;
-				y = 70;
-			} else if (a == 3) {
-				x = 160;
-				y = 70;
-			} else {
-				x = 160;
-				y = 5;
+			if (a == 3) {
+				x = width - 210;
+				y = centerY;
+			};
+		} else if (game.enemies.length == 5) {
+			if (a == 3) {
+				x = width - 210;
+				y = centerY + 32;
+			} else if (a == 4) {
+				x = width - 210;
+				y = centerY - 32;
+			};
+		} else if (game.enemies.length == 6) {
+			if (a == 3) {
+				x = width - 210;
+				y = centerY + 64;
+			} else if (a == 4) {
+				x = width - 210;
+				y = centerY;
+			} else if (a == 5) {
+				x = width - 210;
+				y = centerY - 64;
 			};
 		};
+		x = Math.round(x);
+		y = Math.round(y);
 		if (enemyAnim[a] >= 4) enemyAnim[a] = 0;
 		if (enemy[0] == "slime_big") {
 			ctx.drawImage(slime_big, Math.floor(enemyAnim[a]) * 64, 0, 64, 64, x, y, 64, 64);
