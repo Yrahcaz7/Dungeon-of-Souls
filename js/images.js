8c85ec38513188a9e5fbeb2d74dbd9e2d178e68a
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-10 10:34:09 -0400

    increase max hand size by a lot

--- js/images.js
+++ js/images.js
@@ -301,9 +301,17 @@ function setCardPos() {
 	} else if (length == 6) {
 		game.handPos = [center - 192 + 10, center - 128 + 6, center - 64 + 2, center - 2, center + 64 - 6, center + 128 - 10];
 	} else if (length == 7) {
-		game.handPos = [center - 224 + 39, center - 160 + 26, center - 96 + 13, center - 32, center + 32 - 13, center + 96 - 26, center + 160 - 39];
+		game.handPos = [center - 224 + 42, center - 160 + 28, center - 96 + 14, center - 32, center + 32 - 14, center + 96 - 28, center + 160 - 42];
 	} else if (length == 8) {
-		game.handPos = [center - 256 + 70, center - 192 + 50, center - 128 + 30, center - 64 + 10, center - 10, center + 64 - 30, center + 128 - 50, center + 192 - 70];
+		game.handPos = [center - 256 + 77, center - 192 + 55, center - 128 + 33, center - 64 + 11, center - 11, center + 64 - 33, center + 128 - 55, center + 192 - 77];
+	} else if (length == 9) {
+		game.handPos = [center - 288 + 104, center - 224 + 78, center - 160 + 52, center - 96 + 26, center - 32, center + 32 - 26, center + 96 - 52, center + 160 - 78, center + 224 - 104];
+	} else if (length == 10) {
+		game.handPos = [center - 320 + 135, center - 256 + 105, center - 192 + 75, center - 128 + 45, center - 64 + 15, center - 15, center + 64 - 45, center + 128 - 75, center + 192 - 105, center + 256 - 135];
+	} else if (length == 11) {
+		game.handPos = [center - 352 + 170, center - 288 + 136, center - 224 + 102, center - 160 + 68, center - 96 + 34, center - 32, center + 32 - 34, center + 96 - 68, center + 160 - 102, center + 224 - 136, center + 288 - 170];
+	} else if (length == 12) {
+		game.handPos = [center - 384 + 198, center - 320 + 162, center - 256 + 126, center - 192 + 90, center - 128 + 54, center - 64 + 18, center - 18, center + 64 - 54, center + 128 - 90, center + 192 - 126, center + 256 - 162, center + 320 - 198];
 	};
 };
 
