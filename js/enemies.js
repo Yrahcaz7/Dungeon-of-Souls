704d895253cbcbdc0177e6a864960759f4c824e3
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-12 15:43:54 -0400

    small format fix

--- js/enemies.js
+++ js/enemies.js
@@ -6,7 +6,7 @@ class Enemy {
         this.health = this.maxHealth;
         this.maxShield = Math.round(((Math.random() / 4) + 0.875) * ((power * 5) + 10));
         this.shield = 0;
-        this.attackPower =  Math.round(((power / 2) + 1) * 5);
+        this.attackPower = Math.round(((power / 2) + 1) * 5);
         this.location = game.enemyIndex;
         game.enemyIndex++;
     };
