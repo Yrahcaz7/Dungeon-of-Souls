6db06b94697f13ec1ea2a50bd35806ddb4e1ed04
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-21 16:27:22 -0400

    music time is relative

--- js/game.js
+++ js/game.js
@@ -303,7 +303,7 @@ const gameloop = setInterval(function() {
 }, 100), musicloop = setInterval(function() {
     let time = document.getElementById("music").currentTime;
     if (game.music) {
-        if ((time >= 48 || time == 0) && game.floor < 20) {
+        if ((time > document.getElementById("music").duration - 0.1 || time == 0)) {
             document.getElementById("music").currentTime = 0;
             document.getElementById("music").play();
         };
