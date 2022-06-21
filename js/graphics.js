d684e3970d6e12064ef768f51ec034a17c7fb43a
Yrahcaz7 <74512479+Yrahcaz7@users.noreply.github.com>
2022-06-21 15:54:10 -0400

    fix align

--- js/graphics.js
+++ js/graphics.js
@@ -221,9 +221,9 @@ function foregrounds() {
     if (game.select[0] == "looker" && game.select[1] == 1) draw.imageSector(looker, 15, 0, 16, 16, 362, 3);
 	else draw.imageSector(looker, 0, 0, 16, 16, 362, 3);
 	if (game.select[0] == "looker") draw.image(select.round, 361, 2);
-	if (!game.music) draw.imageSector(music, 15, 0, 16, 16, 344, 3);
-	else draw.imageSector(music, 0, 0, 16, 16, 344, 3);
-	if (game.select[0] == "music") draw.image(select.round, 343, 2);
+	if (!game.music) draw.imageSector(music, 15, 0, 16, 16, 343, 3);
+	else draw.imageSector(music, 0, 0, 16, 16, 343, 3);
+	if (game.select[0] == "music") draw.image(select.round, 342, 2);
 	draw.image(end, 3, 163);
 	if (game.select[0] == "end") draw.image(select.round, 2, 162);
 	draw.image(deck, 3, 182);
